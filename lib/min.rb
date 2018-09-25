b4e7a52844555c41710f3b85723dd34fd414ecf4
Harry Bloch <harryfbloch@gmail.com>
2018-09-25 14:35:20 +0000

    automatically backed up by learn

--- lib/min.rb
+++ lib/min.rb
@@ -10,4 +10,10 @@ require 'pry'
 
 def get_the_min(groceries)
   #code your solution here!
+  itemArray = []
+  groceries.each { |cat , itmArray| 
+    itmArray.each {|item| itmArray << item}
+  }
+  itemArray.sort()
+  itemArray[0]
 end
